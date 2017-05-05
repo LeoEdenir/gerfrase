320417960677e27da60c1c1c1f98108b2b83c13c
LeoEdenir <leonardo.edenirgraciano@hotmail.com>
2017-05-05 09:25:48 -0300

    Add files via upload

---
+++ logica.php
@@ -0,0 +1,10 @@
+<?php
+
+$tratamentos = ['Dom', 'Sir', 'Oráculo'];
+$qualidades  = [' o pistoleiro', ' o rei supremo', ' o like a boss'];
+$lugares      = [' das terras médias', ' de Nárnia', ' das galáxias'];
+$nome       = " Josevaldo";
+
+$tratamento = $tratamentos[rand(0,2)];
+$qualidade = $qualidades[rand(0,2)];
+$lugar = $lugares[rand(0,2)];
